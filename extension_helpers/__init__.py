94bad94f7c9eec6daefe8c2e809c8eb59a4455ba
Thomas Robitaille <thomas.robitaille@gmail.com>
2022-02-05 00:02:46 +0000

    Fix code style

--- extension_helpers/__init__.py
+++ extension_helpers/__init__.py
@@ -1,9 +1,9 @@
+from configparser import ConfigParser
+
 from ._openmp_helpers import add_openmp_flags_if_available
 from ._setup_helpers import get_compiler, get_extensions, pkg_config
 from ._utils import import_file, write_if_different
 from .version import version as __version__
-
-from configparser import ConfigParser
 
 
 def _finalize_distribution_hook(distribution):
@@ -15,6 +15,7 @@ def _finalize_distribution_hook(distribution):
         return
     cfg = ConfigParser()
     cfg.read(config_files[0])
-    if cfg.has_option("extension_helpers", "use_extension_helpers") and cfg.get("extension_helpers", "use_extension_helpers"):
+    if (cfg.has_option("extension_helpers", "use_extension_helpers") and
+            cfg.get("extension_helpers", "use_extension_helpers")):
         extension_helpers_cfg = cfg["extension_helpers"]
         distribution.ext_modules = get_extensions()
